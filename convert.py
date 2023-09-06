2a3bf8deeda0fac8e6bab4ffc844a05f52082784
RadarWarning <44814335+RadarWarning@users.noreply.github.com>
2023-09-06 11:34:08 +0100

    Add files via upload
    Changed layout and titles of output CSV. Improved the collection of data from the input file to ignore any positioning flights and include everything else.

--- convert.py
+++ convert.py
@@ -1,61 +1,75 @@
 import csv
+from datetime import datetime
 
 def navblue_to_myflightbook(input_file, output_file):
     with open(input_file, 'r') as infile, open(output_file, 'w', newline='') as outfile:
         reader = csv.DictReader(infile)
-        fields = ['DateOut', 'Text4', 'From', 'To', 'PIC', 'SIC']
+        fields = ['Date', 'Tail Number', 'Routes', 'PICUS', 'SIC', 'Flight Start', 'Flight End', 'Total Flight Time', 'Landings', 'Comments']
         writer = csv.DictWriter(outfile, fieldnames=fields)
 
         writer.writeheader()
 
-        dateText = ''
-
         for row in reader:
-
-            #Date doesn't appear on every row
+            # Date doesn't appear on every row
             if row['ï»¿RaidoLab_Name'].strip():
                 dateText = row['ï»¿RaidoLab_Name'].strip()
 
-            #Flags
+            # Flags
             flagsString = row['Text4'].split(',')
 
+            if any('P' in flag for flag in flagsString):
+                continue
+            
+            if any('BK' in flag for flag in flagsString):
+                continue
+
             flags = {
-                'P': False, #Positioning
-                'T': False, #Training
-                'X': False #In Command
+                'P': False,  # Positioning
+                'T': False,  # Training
+                'X': False,  # In Command
             }
 
             for flag in flagsString:
                 cleaned_flag = ''.join([char for char in flag if not char.isdigit()])
                 flags[cleaned_flag] = True
             
-            if not flags['T']:
-               continue
-            
-            #Calculate Times
             duration = time_difference(row['TimeText_ActualValidFrom'], row['TimeText_ActualValidToString'])
-
+            
             if duration == 0:
                 continue
-
-            picTime = 0
-            sicTime = duration
             
-            if flags['X']:
-                picTime = duration
+            picusTime = 0
+            sicTime =duration
 
-            #Other fields
+            if flags['X']:
+                picusTime = sicTime
+
+            # Combine From and To into Routes
             srcAirport = row['Text11']
             dstAirport = row['Text12']
+            routes = f"{srcAirport} {dstAirport}"
+
+            # Clean and parse Times
+            flight_start_str = row['TimeText_ActualValidFrom'].strip()
+            flight_end_str = row['TimeText_ActualValidToString'].strip()
+
+            flight_start = datetime.strptime(f"{dateText[:6]}{flight_start_str}", "%d%b%y%H%M")
+            flight_end = datetime.strptime(f"{dateText[:6]}{flight_end_str}", "%d%b%y%H%M")
+
+            reg = row['Text10']
 
             writer.writerow({
-                'DateOut': dateText,
-                'From': srcAirport,
-                'To': dstAirport,
-                'PIC': picTime,
-                'SIC': sicTime
+                'Date': dateText,
+                'Tail Number': reg,
+                'Routes': routes,
+                'PICUS': picusTime/60,
+                'SIC': sicTime/60,
+                'Flight Start': flight_start.strftime("%Y/%m/%d %H:%M"),
+                'Flight End': flight_end.strftime("%Y/%m/%d %H:%M"),
+                'Total Flight Time' : duration/ 60,
+                'Landings' : 1,
             })
-
+            
 def time_difference(time1, time2):
     # Remove non-digit characters
     clean_time1 = ''.join(filter(str.isdigit, time1)).rjust(4, '0')
